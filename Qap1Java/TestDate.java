e272e1c28978f824b6cbdd688ba0d9acf82c5b61
Hunter Saunders <Hunter.Saunders@keyin.com>
2025-02-20 16:54:10 -0330

    Update TestDate.java

--- Qap1Java/TestDate.java
+++ Qap1Java/TestDate.java
@@ -1,6 +1,6 @@
 public class TestDate {
     public static void main(String[] args) {
-        // Create a Date object
+        
         Date date = new Date(27, 1, 2025);
 
         // Print the date using the toString() method
